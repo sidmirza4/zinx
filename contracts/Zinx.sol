7042ab3b7fb18a440f03a4cc5e633ac1343bfbae
Mohd Shahid <sidmirza4@gmail.com>
2022-01-24 21:28:45 +0530

    write tests for zinx

--- contracts/Zinx.sol
+++ contracts/Zinx.sol
@@ -4,54 +4,50 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Zinx {
+  uint public photoCount = 0;
+  
   struct Photo {
-    string id;
     string photoHash;
     string authorName;
-    address author;
+    address payable author;
     string description;
     uint totalDonation;
   }
 
   event Donate (
-    string id,
     address receiver,
     uint amount,
     address donator
   );
 
-  mapping(string => Photo) public photos;
+  mapping(uint => Photo) public photos;
 
-  function uploadPhoto(string memory id, string memory photoHash, string memory authorName, string memory description) public {
-    require(bytes(id).length > 0, "Please provide a photo id");
+  function uploadPhoto(string memory photoHash, string memory authorName, string memory description) public {
     require(bytes(photoHash).length > 0, "Photo hash is not valid");
-    
-    photos[id] = Photo({
-      id: id,
+
+    // adding photo to the key
+    photos[photoCount+1] = Photo({
       photoHash: photoHash,
       authorName: authorName,
-      author: msg.sender,
+      author: payable(msg.sender),
       description: description,
       totalDonation: 0
     });
+
+    // increasing the photoCount
+    photoCount++;
   }
 
-  function getPhotoBy(string memory id) public view returns (Photo memory photo) {
-    return photos[id];
-  }
-
-  function donate(string memory id, uint amount) public payable {
+  function donate(uint id, uint amount) public payable {
     // Throwing an  error is balance is not sufficient
     require(msg.sender.balance > amount, "Insufficient Balance");
-
-    // Checking if id is correct
-    require(bytes(id).length > 0, "Please provide a photo id");
     
     // getting the photo data from the `photos` mapping
-    address receiver = photos[id].author;
+    address payable receiver = photos[id].author;
 
     // throwing an error if user is trying to send money to himself
     require(msg.sender != receiver, "You cannot donate to yourself");
+
 
     // updating donated amount
     photos[id].totalDonation += msg.value;
@@ -59,6 +55,6 @@ contract Zinx {
     console.log("Donating %s amount to %s", msg.value, receiver);
 
     // emitting donate event
-    emit Donate(id, receiver, msg.value, msg.sender);
+    emit Donate(receiver, msg.value, msg.sender);
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,66 @@
-import { expect } from "chai";
+import { assert, expect } from "chai";
+import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("Zinx", function () {
+  let Zinx;
+  let zinx: Contract;
+  let accounts: any[];
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  beforeEach(async () => {
+    accounts = await ethers.getSigners();
+    Zinx = await ethers.getContractFactory("Zinx");
+    zinx = await Zinx.deploy();
+    await zinx.deployed();
+  });
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  it("Should be deployed", async function () {
+    assert(zinx);
+    const photoCount = await zinx.photoCount();
+    expect(photoCount).equal(0);
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  describe("Photos", function () {
+    const newPhoto = {
+      photoHash: "This is the photohash",
+      authorName: "This is author name",
+      description: "This is the description",
+    };
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    this.beforeEach(async function () {
+      await zinx.uploadPhoto(
+        newPhoto.photoHash,
+        newPhoto.authorName,
+        newPhoto.description
+      );
+    });
+
+    it("should be uploaded", async function () {
+      const photoCount = await zinx.photoCount();
+      expect(photoCount).equal(1);
+    });
+
+    it("should be returned by id", async function () {
+      const firstPhoto = await zinx.photos(1);
+      expect(firstPhoto.photoHash).to.equal("This is the photohash");
+      expect(firstPhoto.authorName).to.equal("This is author name");
+      expect(firstPhoto.description).to.equal("This is the description");
+      expect(firstPhoto.author).to.equal(accounts[0].address);
+      expect(firstPhoto.totalDonation).to.equal(0);
+    });
+
+    it("should accept donation", async function () {
+      // revert if owner is sending ether to himself
+      expect(zinx.donate(1, 10)).to.be.revertedWith(
+        "You cannot donate to yourself"
+      );
+
+      // making donation from addr2
+      await zinx.connect(accounts[1]).donate(1, 10);
+
+      // making the transfer
+      // const photo = await zinx.photos(1);
+      // expect(photo.totalDonation.toNumber()).to.equal(10);
+    });
   });
 });
